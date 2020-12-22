01d1c9c2fb669ac6d4b40ab16df45ee222928f2e
priyanshu <priyanshushekhar029@gmail.com>
2020-12-22 16:53:09 +0530

    implementation of bookmark and Share function and some modification on design of Docs Card

--- lib/presentation/widgets/docs_card.dart
+++ lib/presentation/widgets/docs_card.dart
@@ -1,10 +1,24 @@
 import 'package:edualgo_app_demo/core/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:share/share.dart';
 
-class DocsCard extends StatelessWidget {
+class DocsCard extends StatefulWidget {
+
+  @override
+  _DocsCardState createState() => _DocsCardState();
+}
+
+class _DocsCardState extends State<DocsCard> {
+  List<String> listString =List<String>();
+  var bookmarkIcon = FontAwesomeIcons.bookmark;
+
+
   @override
   Widget build(BuildContext context) {
+
+
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: Card(
@@ -52,9 +66,9 @@ class DocsCard extends StatelessWidget {
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
                   'Greyhound divisively hello coldly wonderfully marginally far '
-                  'upon excluding.Greyhound divisively hello coldly wonderfully '
-                  'marginally far upon excluding.Greyhound divisively hello '
-                  'coldly wonderfully marginally far upon excluding.',
+                      'upon excluding.Greyhound divisively hello coldly wonderfully '
+                      'marginally far upon excluding.Greyhound divisively hello '
+                      'coldly wonderfully marginally far upon excluding.',
                   style: TextStyle(
                     fontFamily: 'Recursive',
                     fontSize: 15.0,
@@ -100,20 +114,42 @@ class DocsCard extends StatelessWidget {
                     width: 10.0,
                   ),
                   IconButton(
-                    icon: FaIcon(FontAwesomeIcons.bookmark),
+                    icon: FaIcon(bookmarkIcon),
                     iconSize: 30,
                     color: kThemeColor,
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        bookmarkIcon == FontAwesomeIcons.bookmark ? bookmarkIcon= FontAwesomeIcons.solidBookmark : bookmarkIcon = FontAwesomeIcons.bookmark;
+                      });
+
+                    },
                   ),
                   IconButton(
                     icon: FaIcon(FontAwesomeIcons.shareAltSquare),
                     iconSize: 35,
                     color: kThemeColor,
-                    onPressed: () {},
+                    onPressed: () async {
+
+                      final RenderBox box = context.findRenderObject();
+                      await Share.share('Card Title ',
+                          subject: 'Code',
+                          sharePositionOrigin: box.localToGlobal(Offset.zero)&box.size
+                      );
+//                      listString.add('images/card1.png');
+//                      await Share.shareFiles(
+//                          listString,
+//                          text: 'Card',
+//                          subject: 'Subject',
+//                          sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size
+//                      );
+                    },
                   ),
                 ],
               ),
-              Image.asset('images/card1.png'),
+              Padding(
+                padding: const EdgeInsets.only(left: 10,right: 10.0,bottom: 10.0),
+                child: Image.asset('images/card1.png'),
+              ),
             ],
           ),
         ),
